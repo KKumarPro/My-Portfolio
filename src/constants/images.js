487dc374b5e085d2e33089da8bb0544f1caa3e95
KKumarPro <ak23071977@gmail.com>
2026-01-07 23:28:48 +0530

    Updating images

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,6 +2,7 @@
 import logo from '../assets/1logo.png';
 //import profile from '../assets/greg.jpeg';
 
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
     logo,
     //profile
